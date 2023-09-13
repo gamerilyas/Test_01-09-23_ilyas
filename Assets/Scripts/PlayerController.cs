e871c52fe0a1dba03ba7ac7f2f4d0e633d05e2f7
gamerilyas <ssaciilyas@gmail.com>
2023-09-13 12:05:25 +0200

    13/09/2023
     Quaternion.LookRotation(New Vector3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Variables pour le deplacement
     public CharacterController cc;
     public float moveSpeed;
     public float jumpForce;
     public float gravity;
+    //Vecteur direction souhaitée
     private Vector3 moveDir;
     void Update()
     {
+        //
         moveDir = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDir. y, Input.GetAxis("Vertical"));
 
         if (Input.GetButtonDown("Jump"))
@@ -22,7 +25,7 @@ public class PlayerController : MonoBehaviour
 
         if(moveDir.x != 0 || moveDir.z != 0)
         {
-            transform.rotation = Quaternion.Slerp( transform.rotation, );
+            transform.rotation = Quaternion.Slerp( transform.rotation, Quaternion.LookRotation(New Vector3(moveDirDir.x, 0, moveDir.z )), 0.15f );
         }
         cc.Move(moveDir * Time.deltaTime);
     }
